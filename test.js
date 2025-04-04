f4b0d588658e51accdc73fe38cc5f2447e00da9d
Sujithra Mahendran <smsuji.bca@gmail.com>
2025-04-04 14:53:08 -0400

    changes updated

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
-let light = "yellow";
-if(light == "green"){
-    console.log("Good to go");
-} else if (light == "yellow") {
-    console.log("Wait"); 
+let age = -66;
+
+if(age > 0 && age <= 12){
+    console.log("Child");
+} else if( age >= 13 && age <= 17){
+    console.log("Teenager");
+} else if( age >= 18 && age <= 64) {
+    console.log("Adult");
+} else if(age >= 65) {
+    console.log("Seniors");
 } else {
-    console.log("Stop");
+    console.log("Invalid");
 }
