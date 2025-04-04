81ed41acdbfb4d76bb7cedcb24d57eb9ca8182e4
Sujithra Mahendran <smsuji.bca@gmail.com>
2025-04-04 16:26:02 -0400

    changes updated

--- feature.js
+++ feature.js
@@ -1,1 +1,7 @@
-console.log("feature file");
+let userMem;
+let cartValue = 120;
+if(userMem == false || cartValue >= 100){
+    console.log("they get discount");
+} else {
+    console.log("they dont get discount");
+}
